f10e08add5350cf84c70bfef1169bcb9667c5ef7
Pedro Martins e Sá <pedro.sa@take.net>
2022-09-08 16:08:28 -0300

    feat: types part

--- src/app/Features/AutoTag/tagsHandler.ts
+++ src/app/Features/AutoTag/tagsHandler.ts
@@ -1,6 +1,12 @@
 import { v4 as uuid } from 'uuid';
 import { Settings } from '~/Settings';
-import { BlipActionType, BlipTag } from '~/types';
+import {
+  BlipActionType,
+  BlipContentAction,
+  BlipFlowBlock,
+  BlipTag,
+  BlipTypeTags,
+} from '~/types';
 import { getBlockById, getUniqActions } from '~/Utils';
 
 /**
@@ -12,17 +18,12 @@ export const updateTags = (blockId: string): void => {
   if (!blockId) return;
 
   const block = getBlockById(blockId);
-  const actionsType = getUniqActions(block);
-  const customTags = block.$tags.filter((tag) => isDifferentOfActionTag(tag, actionsType));
-
+  const customTags = block.$tags.filter(isDifferentOfActionTag);
   block.$tags = customTags;
 
-  actionsType.forEach((actionType: BlipActionType) => {
-    const newTag = createTag(actionType);
-    if(newTag){
-      block.$tags.push(newTag);
-    }
-  });
+  setTagsForBlockActions(block);
+  setTagForBlockMessages(block);
+  setTagForUserInput(block);
 };
 
 /**
@@ -30,7 +31,7 @@ export const updateTags = (blockId: string): void => {
  *
  * @param actionType action that will have its tag created
  */
-const createTag = (actionType: BlipActionType): BlipTag => {
+const createTag = (actionType: BlipTypeTags): BlipTag => {
   const tagId = `blip-tag-${uuid()}`;
   const tagColor = getTagColor(actionType);
 
@@ -47,7 +48,7 @@ const createTag = (actionType: BlipActionType): BlipTag => {
  *
  * @param actionType action that will get the color
  */
-const getTagColor = (actionType: BlipActionType): string => {
+const getTagColor = (actionType: BlipTypeTags): string => {
   const matchedAction = Settings.personalTags.filter(
     (tag) => tag.name === actionType
   )[0];
@@ -55,5 +56,52 @@ const getTagColor = (actionType: BlipActionType): string => {
   return matchedAction ? matchedAction?.color : '';
 };
 
-const isDifferentOfActionTag = (tag: BlipTag, actionsType: string[]): boolean => 
-  !actionsType.includes(tag.label);
+const isDifferentOfActionTag = (tag: BlipTag): boolean => {
+  return !(
+    Settings.personalTags.filter(
+      (personalTags) => personalTags.name === tag.label
+    ).length > 0
+  );
+};
+
+const setTagsForBlockActions = (block: BlipFlowBlock): void => {
+  const actionsType = getUniqActions(block);
+
+  actionsType.forEach((actionType: BlipActionType) => {
+    setTag(block, actionType);
+  });
+};
+
+const setTagForUserInput = (block: BlipFlowBlock): void => {
+  const inputAction = getInputAction(block);
+  const hasInputOnBlock = !inputAction.input.bypass;
+
+  if (hasInputOnBlock) {
+    setTag(block, 'UserInput');
+  }
+};
+
+const setTagForBlockMessages = (block: BlipFlowBlock): void => {
+  const blockAction = getActionsFromBlock(block);
+
+  if (blockAction) {
+    const blockHasMessage = blockAction.action.type === 'SendMessage';
+
+    if (blockHasMessage) {
+      setTag(block, 'SendMessage');
+    }
+  }
+};
+
+const getInputAction = (block: BlipFlowBlock): BlipContentAction =>
+  block.$contentActions.find((contentAction) => contentAction['input']);
+
+const getActionsFromBlock = (block: BlipFlowBlock): BlipContentAction =>
+  block.$contentActions.find((contentAction) => contentAction['action']);
+
+const setTag = (block: BlipFlowBlock, actionType: BlipTypeTags): void => {
+  const newTag = createTag(actionType);
+  if (newTag) {
+    block.$tags.push(newTag);
+  }
+};
